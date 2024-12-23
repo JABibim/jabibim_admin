c6e0facf2608ce84d5953194a33d961ca0045919
bitnalchan92 <bitnalchan92@gmail.com>
2024-12-23 15:00:18 +0900

    project init

---
+++ src/main/java/com/jabibim/jabibim_admin/JabibimAdminApplication.java
@@ -0,0 +1,13 @@
+package com.jabibim.jabibim_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JabibimAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JabibimAdminApplication.class, args);
+    }
+
+}
