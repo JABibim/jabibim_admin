75649be774619aa27716fbab9f50a4f2bc9871f6
ChoiGoeunn <rhdms085@gamil.com>
2025-01-03 16:46:36 +0900

    [feat] 개인정보처리방침 글등록 완료

--- src/main/java/com/jabibim/admin/controller/PolicyController.java
+++ src/main/java/com/jabibim/admin/controller/PolicyController.java
@@ -1,12 +1,27 @@
 package com.jabibim.admin.controller;
 
+import com.jabibim.admin.domain.Privacy;
+import com.jabibim.admin.service.PrivacyService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value="/policy")
 public class PolicyController {
+    private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+
+    private PrivacyService privacyService;
+
+    public PolicyController(PrivacyService privacyService) {
+        this.privacyService = privacyService;
+    }
 
     @GetMapping(value="/privacy")
     public String privacyList() {
@@ -16,6 +31,13 @@ public class PolicyController {
     @GetMapping(value="/privacy/write")
     public String privacyWrite() {
         return "policy/privacy/privacy_write";
+    }
+
+    @PostMapping(value="/privacy/add")
+    public String addPolicy(Privacy privacy, HttpServletRequest request) {
+        privacyService.insertPrivacy(privacy);
+        logger.info(privacy.toString());
+        return "redirect:/policy/privacy";
     }
 
     @GetMapping(value="/privacy/detail")
