4c5df57cccdb8a82f5240c34609936e16ee518af
ChoiGoeunn <rhdms085@gamil.com>
2025-01-08 11:44:40 +0900

    [feat] 서비스이용약관 list, insert

---
+++ src/main/java/com/jabibim/admin/service/TermServiceImpl.java
@@ -0,0 +1,49 @@
+package com.jabibim.admin.service;
+
+import com.jabibim.admin.domain.Term;
+import com.jabibim.admin.mybatis.mapper.TermMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class TermServiceImpl implements TermService {
+    private TermMapper dao;
+
+    public TermServiceImpl(TermMapper dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public int getListCount() {
+        return dao.getListCount();
+    }
+
+    @Override
+    public List<Term> getTermList(int page, int limit) {
+        // double i =1/0;
+        HashMap<String, Integer> map = new HashMap<>();
+
+        int startRow = (page - 1) * limit + 1;
+        int endRow = startRow + limit - 1;
+
+        map.put("start", startRow);
+        map.put("end", endRow);
+
+        return dao.getTermList(map);
+    }
+
+    @Override
+    public void insertTerm(Term term) {
+        dao.insertTerm(term);
+    }
+
+    @Override
+    public Term getLatestTermPolicy() {
+        // 최신 본문을 데이터베이스에서 가져옴
+        return dao.getLatestTermPolicy();
+    }
+
+
+}
