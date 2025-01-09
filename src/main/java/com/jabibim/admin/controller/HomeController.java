a46af22e1ad29914dee36a6b7d6e12a474e6b5c8
Jang9721 <imsgchang97@gmail.com>
2025-01-09 16:10:21 +0900

    [fix] ADMIN 로그인시 오류 해결

--- src/main/java/com/jabibim/admin/controller/HomeController.java
+++ src/main/java/com/jabibim/admin/controller/HomeController.java
@@ -1,43 +1,23 @@
 package com.jabibim.admin.controller;
 
-
-import com.jabibim.admin.domain.Teacher;
-import com.jabibim.admin.service.TeacherServiceImpl;
-import jakarta.servlet.http.HttpSession;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    private final TeacherServiceImpl teacherServiceImpl;
-
-    public HomeController(TeacherServiceImpl teacherServiceImpl) {
-        this.teacherServiceImpl = teacherServiceImpl;
-    }
-
     @GetMapping(value = "/")
     public String home() {
         return "redirect:/login";
     }
 
     @GetMapping(value = "/dashboard")
-    public String dashBoard(HttpSession session, Model model) {
-        String teacherId = (String) session.getAttribute("id");
-        Teacher teacherInfo = teacherServiceImpl.getTeacherById(teacherId);
-        System.out.println("teacherInfo============" + teacherInfo);
-
-        model.addAttribute("teacherImgName", teacherInfo.getTeacherImgName());
+    public String dashBoard() {
         return "dashboard";
     }
-
-
 
     @GetMapping("/message")
     public String message(HttpServletRequest request) {
         return "message/message_modal";
     }
-
-
 }
