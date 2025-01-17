824b6fb11f434fd7964d0bd7b97843ef0320d21b
DESKTOP-U7D63L1\MO <salbul.mo91@gmail.com>
2025-01-17 16:02:02 +0900

    [feat] 로그인 요청을 받을 때 사용할 유저 정보를 담을 dto

--- src/main/java/com/jabibim/admin/front/dto/LoginRequest.java
+++ src/main/java/com/jabibim/admin/front/dto/LoginRequest.java
@@ -3,11 +3,14 @@ package com.jabibim.admin.front.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
-public class LoginRequest {
+public class LoginRequest implements Serializable {
 
   @NotBlank(message = "이메일은 필수입니다.") // 공백, null, 빈문자열 불가
   private String email;
