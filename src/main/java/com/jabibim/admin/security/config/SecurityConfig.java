ddb0b2d55feb43ce13d516a623b68804c9b60a0d
DESKTOP-U7D63L1\MO <salbul.mo91@gmail.com>
2025-01-17 16:43:27 +0900

    [fix] 세션 로그인 처리를 위해 일부 변경

--- src/main/java/com/jabibim/admin/security/config/SecurityConfig.java
+++ src/main/java/com/jabibim/admin/security/config/SecurityConfig.java
@@ -16,6 +16,8 @@ import com.jabibim.admin.security.provider.FormAuthenticationProvider;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -23,48 +25,56 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final FormAuthenticationProvider formAuthenticationProvider;
-    private final AuthenticationSuccessHandler successHandler;
-    private final AuthenticationFailureHandler failureHandler;
-    private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
+        private final FormAuthenticationProvider formAuthenticationProvider;
+        private final AuthenticationSuccessHandler successHandler;
+        private final AuthenticationFailureHandler failureHandler;
+        private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        System.out.println("=================> SecurityConfig");
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                System.out.println("=================> SecurityConfig");
 
-        http
-                .authorizeHttpRequests(auth -> auth
-                        // 정적 자원 permitAll ( 성능 고려, security 적용 안함 )
-                        .requestMatchers("/css/**", "/img/**", "/js/**", "/niceAdmin/**", "/sql/**").permitAll()
-                        // login page 경로는 모두 permitAll
-                        .requestMatchers("/", "/login*").permitAll()
-                        // 권한별 접근 제한 설정
-                        .requestMatchers("/setting").hasAuthority("ROLE_ADMIN")
-                        .anyRequest().authenticated())
-                .formLogin(form -> form
-                        // loginPage 경로 설정
-                        .loginPage("/login").permitAll()
-                        // authenticationDetailsSource 설정 ( 부가정보 셋팅할 수 있음 )
-                        .authenticationDetailsSource(authenticationDetailsSource)
-                        // usernameParameter, passwordParameter 설정 ( name property가 email, password인
-                        // input 태그를 찾아서 처리 )
-                        .usernameParameter("email")
-                        .passwordParameter("password")
-                        // 성공 처리 Handler
-                        .successHandler(successHandler)
-                        // 실패 처리 Handler
-                        .failureHandler(failureHandler))
-                // authenticationProvider 설정
-                .authenticationProvider(formAuthenticationProvider)
-                // exceptionHandling 설정 ( 인증 받은 상태에서 호출되는거! 로그인한 사용자가 접근할 수 없는 페이지에 접근했을 때 )
-                .exceptionHandling(exception -> exception.accessDeniedHandler(new FormAccessDeniedHandler("/denied")));
+                http
+                    .securityMatcher(new NegatedRequestMatcher(new AntPathRequestMatcher("/api/**")))  // API 요청 제외
 
-        http.logout((logout) -> logout
-                .logoutSuccessUrl("/login")
-                .logoutUrl("/logout")
-                .invalidateHttpSession(true)
-                .deleteCookies("remember-me", "JSESSION_ID"));
+                                .authorizeHttpRequests(auth -> auth
+                                                // 정적 자원 permitAll ( 성능 고려, security 적용 안함 )
+                                                .requestMatchers("/css/**", "/img/**", "/js/**", "/niceAdmin/**",
+                                                                "/sql/**")
+                                                .permitAll()
+                                                // login page 경로는 모두 permitAll
+                                                .requestMatchers("/", "/login*").permitAll()
+                                                // 권한별 접근 제한 설정
+                                                .requestMatchers("/setting").hasAuthority("ROLE_ADMIN")
+                                                .anyRequest().authenticated())
+                                .formLogin(form -> form
+                                                // loginPage 경로 설정
+                                                .loginPage("/login").permitAll()
+                                                .loginProcessingUrl("/loginprocess")
 
-        return http.build();
-    }
+                                                // authenticationDetailsSource 설정 ( 부가정보 셋팅할 수 있음 )
+                                                .authenticationDetailsSource(authenticationDetailsSource)
+                                                // usernameParameter, passwordParameter 설정 ( name property가 email,
+                                                // password인
+                                                // input 태그를 찾아서 처리 )
+                                                .usernameParameter("email")
+                                                .passwordParameter("password")
+                                                // 성공 처리 Handler
+                                                .successHandler(successHandler)
+                                                // 실패 처리 Handler
+                                                .failureHandler(failureHandler))
+                                // authenticationProvider 설정
+                                .authenticationProvider(formAuthenticationProvider)
+                                // exceptionHandling 설정 ( 인증 받은 상태에서 호출되는거! 로그인한 사용자가 접근할 수 없는 페이지에 접근했을 때 )
+                                .exceptionHandling(exception -> exception
+                                                .accessDeniedHandler(new FormAccessDeniedHandler("/denied")));
+
+                http.logout((logout) -> logout
+                                .logoutSuccessUrl("/login")
+                                .logoutUrl("/logout")
+                                .invalidateHttpSession(true)
+                                .deleteCookies("remember-me", "JSESSION_ID"));
+
+                return http.build();
+        }
 }
