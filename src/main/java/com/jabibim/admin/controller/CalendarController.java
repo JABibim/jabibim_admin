0a6892583a3f7d2273a0d779fef6ca80e13207b3
ChoiGoeunn <rhdms085@gamil.com>
2025-01-20 12:07:35 +0900

    [feat] calendar 화면에 출력

---
+++ src/main/java/com/jabibim/admin/controller/CalendarController.java
@@ -0,0 +1,21 @@
+package com.jabibim.admin.controller;
+
+
+import com.jabibim.admin.dto.CourseListDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping(value="/calendar")
+public class CalendarController {
+
+    @GetMapping(value="")
+    public String boardList(
+    ) {
+        return "calendar/calendar";
+    }
+}
