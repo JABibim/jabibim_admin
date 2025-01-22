93ba77a89a77bdb2ccd72ca2de4878e91d3c5a4f
ChoiGoeunn <rhdms085@gamil.com>
2025-01-22 12:10:16 +0900

    [fix] 충돌해결

--- src/main/java/com/jabibim/admin/controller/CalendarController.java
+++ src/main/java/com/jabibim/admin/controller/CalendarController.java
@@ -1,13 +1,7 @@
 package com.jabibim.admin.controller;
 
-
-import com.jabibim.admin.dto.CourseListDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
-
 
 @Controller
 @RequestMapping(value="/calendar")
@@ -16,6 +10,7 @@ public class CalendarController {
     @GetMapping(value="")
     public String boardList(
     ) {
-        return "calendar/calendar3";
+        return "calendar/calendar5";
     }
+
 }
