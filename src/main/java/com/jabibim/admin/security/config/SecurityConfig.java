5bb2943d2456aa2558c44468b8f3c83550c567bc
bitnalchan92 <bitnalchan92@gmail.com>
2025-02-11 19:57:18 +0900

    [feat] s3 로직 변경 및 프론트 화면에서 필요한 로직 추가

--- src/main/java/com/jabibim/admin/security/config/SecurityConfig.java
+++ src/main/java/com/jabibim/admin/security/config/SecurityConfig.java
@@ -12,22 +12,15 @@ import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
-import com.jabibim.admin.security.handler.FormAccessDeniedHandler;
 import com.jabibim.admin.security.provider.FormAuthenticationProvider;
 
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 
@@ -48,8 +41,6 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        System.out.println("=================> SecurityConfig");
-
         http
                 .formLogin((login) -> login.loginPage("/login"));
 
@@ -69,7 +60,7 @@ public class SecurityConfig {
                         // 정적 자원 permitAll ( 성능 고려, security 적용 안함 )
                         .requestMatchers("/css/**", "/img/**", "/js/**", "/niceAdmin/**", "/sql/**").permitAll()
                         // login page 경로는 모두 permitAll
-                        .requestMatchers("/", "/login*","/oauth2/**" ).permitAll()
+                        .requestMatchers("/", "/login*", "/oauth2/**").permitAll()
                         // 웹훅 EndPoint 접근 제한 해제
                         .requestMatchers("/api/public/**", "/webhook/**").permitAll()
                         // 권한별 접근 제한 설정
@@ -92,13 +83,12 @@ public class SecurityConfig {
                 // exceptionHandling 설정 ( 인증 받은 상태에서 호출되는거! 로그인한 사용자가 접근할 수 없는 페이지에 접근했을 때 )
                 .exceptionHandling(exception -> exception.accessDeniedHandler(new FormAccessDeniedHandler("/denied")));
 
-                http.logout((logout) -> logout
-                                .logoutSuccessUrl("/login")
-                                .logoutUrl("/logout")
-                                .invalidateHttpSession(true)
-                                .deleteCookies("remember-me", "JSESSION_ID"));
+        http.logout((logout) -> logout
+                .logoutSuccessUrl("/login")
+                .logoutUrl("/logout")
+                .invalidateHttpSession(true)
+                .deleteCookies("remember-me", "JSESSION_ID"));
 
-                return http.build();
-        }
-
+        return http.build();
+    }
 }
--- src/main/java/com/jabibim/admin/service/CourseService.java
+++ src/main/java/com/jabibim/admin/service/CourseService.java
@@ -2,14 +2,17 @@ package com.jabibim.admin.service;
 
 import java.util.List;
 
+import com.jabibim.admin.dto.ClassDetailVO;
 import com.jabibim.admin.dto.CourseDetailVO;
 import com.jabibim.admin.dto.CourseInfoVO;
 import com.jabibim.admin.dto.PurchasedCourseVO;
 
 public interface CourseService {
-  List<CourseInfoVO> getCourseInfoList(String academyId);
+    List<CourseInfoVO> getCourseInfoList(String academyId);
 
-  CourseDetailVO getCourseDetail(String courseId, String academyId);
+    CourseDetailVO getCourseDetail(String courseId, String academyId);
 
-  List<PurchasedCourseVO> getPurchasedCourseList(String studentId, String academyId);
+    List<PurchasedCourseVO> getPurchasedCourseList(String studentId, String academyId);
+
+    ClassDetailVO getClassDetail(String classId, String academyId);
 }
