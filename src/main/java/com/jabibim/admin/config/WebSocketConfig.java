76fa2c9d07bcf58a96a5d64190e2b78de61a1b1a
bitnalchan92 <bitnalchan92@gmail.com>
2025-02-13 18:30:46 +0900

    [feat] websocket config 수정

--- src/main/java/com/jabibim/admin/config/WebSocketConfig.java
+++ src/main/java/com/jabibim/admin/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.jabibim.admin.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Value("${websocket.allow-origin}")
+    private String WEBSOCKET_ALLOW_ORIGIN;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -19,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:9500") // 허용할 도메인만 명시
+                .setAllowedOrigins(WEBSOCKET_ALLOW_ORIGIN) // 허용할 도메인만 명시
                 .withSockJS();
     }
 }
